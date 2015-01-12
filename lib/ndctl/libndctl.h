63dac75c340ab20d612512de94796551ac88341d
Dan Williams <dan.j.williams@intel.com>
2015-01-12 13:24:36 -0500

    ndctl: add Vendor Specifc DSM helpers
    Provide a stable api for submitting vendor-specific commands to an
    NVDIMM.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Tested-by: Ross Zwisler <ross.zwisler@linux.intel.com>

--- lib/ndctl/libndctl.h
+++ lib/ndctl/libndctl.h
@@ -136,6 +136,12 @@ struct ndctl_dimm *ndctl_dimm_get_by_handle(struct ndctl_bus *bus,
 		unsigned int handle);
 
 struct ndctl_cmd;
+struct ndctl_cmd *ndctl_dimm_cmd_new_vendor_specific(struct ndctl_dimm *dimm,
+		size_t input_size, size_t output_size);
+ssize_t ndctl_cmd_vendor_set_input(struct ndctl_cmd *cmd, void *buf,
+		unsigned int len);
+ssize_t ndctl_cmd_vendor_get_output(struct ndctl_cmd *cmd, void *buf,
+		unsigned int len);
 struct ndctl_cmd *ndctl_dimm_cmd_new_cfg_size(struct ndctl_dimm *dimm);
 struct ndctl_cmd *ndctl_dimm_cmd_new_cfg_read(struct ndctl_cmd *cfg_size);
 struct ndctl_cmd *ndctl_dimm_cmd_new_cfg_write(struct ndctl_cmd *cfg_read);
