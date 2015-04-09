bee96882f8f1e3d9e46d3a47d3623de9c41601b1
Dan Williams <dan.j.williams@intel.com>
2015-04-08 23:21:28 -0400

    ndctl: cleanup stray debug print
    Reported-by: Nicholas Moulin <nicholas.w.moulin@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- lib/libndctl.c
+++ lib/libndctl.c
@@ -1523,8 +1523,6 @@ NDCTL_EXPORT struct ndctl_cmd *ndctl_dimm_cmd_new_vendor_specific(
 		return NULL;
 	}
 
-	fprintf(stderr, "%s: in: %zd out: %zd\n", __func__, input_size,
-			output_size);
 	size = sizeof(*cmd) + sizeof(struct nfit_cmd_vendor_hdr)
 		+ sizeof(struct nfit_cmd_vendor_tail) + input_size
 		+ output_size;
