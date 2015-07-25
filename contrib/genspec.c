27792e104878c9ecd6ddb84a62903d197ac07413
Dan Williams <dan.j.williams@intel.com>
2015-07-25 17:20:45 -0400

    ndctl: rpm spec file fixes for koji
    1/ Update genspec to place the commit id directly in the spec file
       rather than running git live to retrieve the current HEAD.
    
    2/ Update BuildRequires
    
    With these updates koji can successfully build ndctl.
    
    http://koji.fedoraproject.org/koji/taskinfo?taskID=10479814
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- contrib/genspec.c
+++ contrib/genspec.c
@@ -2,13 +2,20 @@
 #include <string.h>
 #include "../config.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
 	char buf[1024];
+
+	if (argc != 2) {
+		fprintf(stderr, "commit id must be specified\n");
+		return 1;
+	}
 
 	while (fgets(buf, sizeof(buf), stdin))
 		if (strncmp("Version:", buf, 8) == 0)
 			fprintf(stdout, "Version:        %s\n", VERSION);
+		else if (strncmp("%global gitcommit", buf, 17) == 0)
+			fprintf(stdout, "%%global gitcommit %s\n", argv[1]);
 		else
 			fprintf(stdout, "%s", buf);
 
