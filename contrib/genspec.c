f343e10fffb8866ad4851929a7609d94efee3796
Dan Williams <dan.j.williams@intel.com>
2015-08-03 17:55:32 -0400

    ndctl: update genspec to output per-distro flavored spec files
    The Red Hat environment prefers:
    ndctl, ndctl-libs, ndctl-devel
    
    The SUSE environment prefers:
    ndctl, libndctl3, libndctl-devel
    
    SUSE is more strict about the "License:" field.  Finally, SUSE does not
    seem to ship a /usr/share/license directory, so suppress "%license" for
    SUSE.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- contrib/genspec.c
+++ contrib/genspec.c
@@ -2,22 +2,50 @@
 #include <string.h>
 #include "../config.h"
 
+static char *lname[] = {
+	"ndctl-libs", "libndctl3",
+};
+
+static char *dname[] = {
+	"ndctl-devel", "libndctl-devel",
+};
+
+static int license[] = {
+	1, 0,
+};
+
 int main(int argc, char **argv)
 {
+	const char *commit = argv[1];
 	char buf[1024];
+	int os;
 
-	if (argc != 2) {
-		fprintf(stderr, "commit id must be specified\n");
+	if (argc != 3) {
+		fprintf(stderr, "commit id and OS must be specified\n");
 		return 1;
 	}
 
-	while (fgets(buf, sizeof(buf), stdin))
+	if (strcmp(argv[2], "rhel") == 0)
+		os = 0;
+	else if (strcmp(argv[2], "sles") == 0)
+		os = 1;
+	else
+		return 1;
+
+	while (fgets(buf, sizeof(buf), stdin)) {
 		if (strncmp("Version:", buf, 8) == 0)
 			fprintf(stdout, "Version:        %s\n", VERSION);
 		else if (strncmp("%global gitcommit", buf, 17) == 0)
-			fprintf(stdout, "%%global gitcommit %s\n", argv[1]);
+			fprintf(stdout, "%%global gitcommit %s\n", commit);
+		else if (strncmp("%define lname", buf, 12) == 0)
+			fprintf(stdout, "%%define lname %s\n", lname[os]);
+		else if (strncmp("%define dname", buf, 12) == 0)
+			fprintf(stdout, "%%define dname %s\n", dname[os]);
+		else if (strncmp("%license", buf, 8) == 0 && !license[os])
+			/* skip */;
 		else
 			fprintf(stdout, "%s", buf);
+	}
 
 	return 0;
 }
--- contrib/rpmbuild.sh
+++ contrib/rpmbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 $(dirname $0)/make-git-snapshot.sh
 make -C $(dirname $0)
-rpmbuild -bb $(dirname $0)/ndctl.spec
+rpmbuild -bb $(dirname $0)/rhel/ndctl.spec
