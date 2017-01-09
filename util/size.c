ee4151c4686f6c56595168748580e6b704b25c72
Dan Williams <dan.j.williams@intel.com>
2017-01-09 11:01:55 -0800

    ndctl: return unit size from parse_size64()
    In preparation for suggesting namespace sizes that satisfy alignment
    requirements, add the ability to track the size suffix. The suggestions
    will be aligned to the size suffix by default.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- util/size.c
+++ util/size.c
@@ -2,7 +2,8 @@
 #include <limits.h>
 #include <util/size.h>
 
-unsigned long long parse_size64(const char *str)
+static unsigned long long __parse_size64(const char *str,
+		unsigned long long *units)
 {
 	unsigned long long val, check;
 	char *end;
@@ -14,29 +15,44 @@ unsigned long long parse_size64(const char *str)
 	switch (*end) {
 		case 'k':
 		case 'K':
+			if (units)
+				*units = SZ_1K;
 			val *= SZ_1K;
 			end++;
 			break;
 		case 'm':
 		case 'M':
+			if (units)
+				*units = SZ_1M;
 			val *= SZ_1M;
 			end++;
 			break;
 		case 'g':
 		case 'G':
+			if (units)
+				*units = SZ_1G;
 			val *= SZ_1G;
 			end++;
 			break;
 		case 't':
 		case 'T':
+			if (units)
+				*units = SZ_1T;
 			val *= SZ_1T;
 			end++;
 			break;
 		default:
+			if (units)
+				*units = 1;
 			break;
 	}
 
 	if (val < check || *end != '\0')
 		val = ULLONG_MAX;
 	return val;
+}
+
+unsigned long long parse_size64(const char *str)
+{
+	return __parse_size64(str, NULL);
 }
