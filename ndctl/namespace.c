pmem/ndctl

commit 61e82a3d7bc8596acb8bcb9f381e63e6a1ffa8e7

ndctl, create-namespace: support sector size settings for pmem
Starting with v1.2 namespace label support it is permissible for pmem
namespaces to have a sector size. Per UEFI 2.7 the default should be 4K
for inter-OS compatibility.

Reported-by: Juston Li <juston.li@intel.com>
Signed-off-by: Dan Williams <dan.j.williams@intel.com>

ndctl/namespace.c
@@ -248,12 +248,6 @@ static int set_defaults(enum namespace_action mode)
 			error("invalid sector size: %s\n", param.sector_size);
 			rc = -EINVAL;
 		}
-
-		if (param.type && param.mode && strcmp(param.type, "pmem") == 0
-				&& strcmp(param.mode, "safe") != 0) {
-			error("'pmem' namespaces do not support setting 'sector size'\n");
-			rc = -EINVAL;
-		}
 	} else if (!param.reconfig
 			&& ((param.type && strcmp(param.type, "blk") == 0)
 				|| (param.mode
@@ -360,7 +354,7 @@ static int setup_namespace(struct ndctl_region *region,
 		try(ndctl_namespace, set_size, ndns, p->size);
 	}
 
-	if (ndctl_namespace_get_type(ndns) == ND_DEVICE_NAMESPACE_BLK)
+	if (p->sector_size && p->sector_size < UINT_MAX)
 		try(ndctl_namespace, set_sector_size, ndns, p->sector_size);
 
 	uuid_generate(uuid);
@@ -394,6 +388,13 @@ static int setup_namespace(struct ndctl_region *region,
 	} else if (p->mode == NDCTL_NS_MODE_SAFE) {
 		struct ndctl_btt *btt = ndctl_region_get_btt_seed(region);
 
+		/*
+		 * Handle the case of btt on a pmem namespace where the
+		 * pmem kernel support is pre-v1.2 namespace labels
+		 * support (does not support sector size settings).
+		 */
+		if (p->sector_size == UINT_MAX)
+			p->sector_size = 4096;
 		try(ndctl_btt, set_uuid, btt, uuid);
 		try(ndctl_btt, set_sector_size, btt, p->sector_size);
 		try(ndctl_btt, set_namespace, btt, ndns);
@@ -632,11 +633,23 @@ static int validate_namespace_options(struct ndctl_region *region,
 
 		if (btt)
 			p->sector_size = ndctl_btt_get_sector_size(btt);
-		else if (ndctl_namespace_get_type(ndns)
-				== ND_DEVICE_NAMESPACE_BLK)
+		else
 			p->sector_size = ndctl_namespace_get_sector_size(ndns);
-		else if (p->mode == NDCTL_NS_MODE_SAFE)
-				p->sector_size = 4096;
+	} else {
+		struct ndctl_namespace *seed;
+
+		seed = ndctl_region_get_namespace_seed(region);
+		if (ndctl_namespace_get_type(seed) == ND_DEVICE_NAMESPACE_BLK)
+			debug("%s: set_defaults() should preclude this?\n",
+				ndctl_region_get_devname(region));
+		/*
+		 * Pick a default sector size for a pmem namespace based
+		 * on what the kernel supports.
+		 */
+		if (ndctl_namespace_get_num_sector_sizes(seed) == 0)
+			p->sector_size = UINT_MAX;
+		else
+			p->sector_size = 512;
 	}
 
 	if (param.map) {

commit fd374ffd240f894b2cec6265667f5309e4ac903f

ndctl, namespace: fix sector size default when switching modes
Maurice reports:

    I do have a question about specific behavior I observed. Before I was
    able to configure an NVDIMM into any mode from any mode. With these
    patchs if I go to sector mode and try going to memory or dax mode it
    fails with:

    # ndctl create-namespace -f -e namespace0.0 -m memory
      Error: namespace0.0: sector_size: 4096 not supported
    failed to reconfigure namespace: Invalid argument

This results from the fact that we pick up the default sector size from
the current btt. However, if the next mode is not btt we should fall
back to the native sector size of the namespace.

Validate this conversion with an extension to the 'sector-mode.sh' test.

Fixes: 61e82a3d7bc8 ("ndctl, create-namespace: support sector size...")
Reported-by: Maurice A. Saldivar <maurice.a.saldivar@hpe.com>
Tested-by: Maurice A. Saldivar <maurice.a.saldivar@hpe.com>
Signed-off-by: Dan Williams <dan.j.williams@intel.com>

ndctl/namespace.c
@@ -653,7 +653,12 @@ static int validate_namespace_options(struct ndctl_region *region,
 	} else if (ndns) {
 		struct ndctl_btt *btt = ndctl_namespace_get_btt(ndns);
 
-		if (btt)
+		/*
+		 * If the target mode is still 'safe' carry forward the
+		 * sector size, otherwise fall back to what the
+		 * namespace supports.
+		 */
+		if (btt && p->mode == NDCTL_NS_MODE_SAFE)
 			p->sector_size = ndctl_btt_get_sector_size(btt);
 		else
 			p->sector_size = ndctl_namespace_get_sector_size(ndns);

test/sector-mode.sh
@@ -45,6 +45,8 @@ NAMESPACE=$($NDCTL list $BUS1 -N | jq -r "$query")
 REGION=$($NDCTL list -R --namespace=$NAMESPACE | jq -r ".dev")
 echo 0 > /sys/bus/nd/devices/$REGION/read_only
 $NDCTL create-namespace -e $NAMESPACE -m sector -f -l 4K
+$NDCTL create-namespace -e $NAMESPACE -m dax -f -a 4K
+$NDCTL create-namespace -e $NAMESPACE -m sector -f -l 4K
 
 $NDCTL disable-region $BUS all
 $NDCTL disable-region $BUS1 all