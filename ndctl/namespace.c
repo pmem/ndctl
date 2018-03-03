pmem/ndctl

commit 0c31ce0a287588062c24445b7d7995f7a9ef49e6

ndctl, create-namespace: fix --align= default for nfit_test
Vishal reports:

    (after ndctl create-namepsace -t pmem --bus=nfit_test.0) I'm seeing that
    it is created as a memory mode namespace, but after cycling it, it comes
    up as raw and dmesg has:

        pfn7.0: bad offset: 0x2af000 dax disabled align: 0x200000

This arises from the hacks that nfit_test uses to emulate pmem with
vmalloc(). Setting the alignment to 4K avoids these problems.

For casual testing try to detect the nfit_test case and adjust the
default alignment accordingly.

Reported-by: Vishal Verma <vishal.l.verma@intel.com>
Signed-off-by: Dan Williams <dan.j.williams@intel.com>

ndctl/namespace.c
@@ -563,6 +563,19 @@ static int validate_namespace_options(struct ndctl_region *region,
 			return -ENXIO;
 		}
 
+		/*
+		 * Fallback to a 4K default alignment if the region is
+		 * not 2MB (typical default) aligned. This mainly helps
+		 * the nfit_test use case where it is backed by vmalloc
+		 * memory.
+		 */
+		if (param.align_default && (ndctl_region_get_resource(region)
+					& (SZ_2M - 1))) {
+			debug("%s: falling back to a 4K alignment\n",
+					region_name);
+			p->align = SZ_4K;
+		}
+
 		switch (p->align) {
 		case SZ_4K:
 		case SZ_2M:

commit 4560ed7733cf9bcc101f297b2d99b0931a7d5fc6

ndctl, create-namespace: fix minimum alignment detection
Commit 0c31ce0a2875 "ndctl, create-namespace: fix --align= default..."
tried to make casual testing with nfit_test easier by auto-detecting the
alignment. However, this auto-detection always falls back to 4K
alignment on kernels that do not export the region 'resource' attribute.
Those kernels, like v4.9.4, may also be missing this fix: commit
cd755677d944 "libnvdimm, pfn: fix memmap reservation size versus 4K
alignment".

Given the chance to fail to initialize a namespace in the 4K case on
older kernels, be more conservative about falling back to 4K alignment.

Link: https://github.com/pmem/ndctl/issues/34
Reported-by: Maciej Ramotowski <maciej.ramotowski@intel.com>
Fixes: 0c31ce0a2875 ("ndctl, create-namespace: fix --align= default...")
Signed-off-by: Dan Williams <dan.j.williams@intel.com>

ndctl/namespace.c
@@ -462,7 +462,7 @@ static int validate_namespace_options(struct ndctl_region *region,
 		struct ndctl_namespace *ndns, struct parsed_parameters *p)
 {
 	const char *region_name = ndctl_region_get_devname(region);
-	unsigned long long size_align = SZ_4K, units = 1;
+	unsigned long long size_align = SZ_4K, units = 1, resource;
 	unsigned int ways;
 	int rc = 0;
 
@@ -563,8 +563,9 @@ static int validate_namespace_options(struct ndctl_region *region,
 		 * the nfit_test use case where it is backed by vmalloc
 		 * memory.
 		 */
-		if (param.align_default && (ndctl_region_get_resource(region)
-					& (SZ_2M - 1))) {
+		resource = ndctl_region_get_resource(region);
+		if (param.align_default && resource < ULLONG_MAX
+				&& (resource & (SZ_2M - 1))) {
 			debug("%s: falling back to a 4K alignment\n",
 					region_name);
 			p->align = SZ_4K;