pmem/ndctl

commit f7d3de80a121054f8a34ebcedc3b6571163dece0

ndctl: support machines without numa
Currently when using ndctl with kernels compiled with CONFIG_NUMA=n,
the ndctl command fails because it fails to find the numa_node attribute
in systems.

Just assume the default, zero, in these cases rather than fail.

Link: https://github.com/pmem/ndctl/pull/9
Signed-off-by: Diego Dompe <dompe@hpe.com>
[djbw: minor style fixups]
Reviewed-by: Jeff Moyer <jmoyer@redhat.com>
Signed-off-by: Dan Williams <dan.j.williams@intel.com>

ndctl/lib/libndctl.c
@@ -2780,9 +2780,8 @@ static void *add_namespace(void *parent, int id, const char *ndns_base)
 	ndns->raw_mode = strtoul(buf, NULL, 0);
 
 	sprintf(path, "%s/numa_node", ndns_base);
-	if (sysfs_read_attr(ctx, path, buf) < 0)
-		goto err_read;
-	ndns->numa_node = strtol(buf, NULL, 0);
+	if (sysfs_read_attr(ctx, path, buf) == 0)
+		ndns->numa_node = strtol(buf, NULL, 0);
 
 	switch (ndns->type) {
 	case ND_DEVICE_NAMESPACE_BLK:

commit f39129f288c0d9182849ac3f8d02cb191a5bc556

ndctl: don't print erroneous namespace numa_nodes
If the kernel has CONFIG_NUMA unset namespaces in sysfs will lack a
numa_node attribute.  In such cases ndctl will report a value of 0 for the
namespace numa_node in 'ndctl list'.  Instead of reporting potentially bad
data just hide the numa_node field if it is unsupported.

Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
Fixes: commit f7d3de80a121 ("ndctl: support machines without numa")
Reviewed-by: Dan Williams <dan.j.williams@intel.com>
Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>

ndctl/lib/libndctl.c
@@ -3008,6 +3008,8 @@ static void *add_namespace(void *parent, int id, const char *ndns_base)
 	sprintf(path, "%s/numa_node", ndns_base);
 	if (sysfs_read_attr(ctx, path, buf) == 0)
 		ndns->numa_node = strtol(buf, NULL, 0);
+	else
+		ndns->numa_node = -1;
 
 	sprintf(path, "%s/holder_class", ndns_base);
 	if (sysfs_read_attr(ctx, path, buf) == 0)