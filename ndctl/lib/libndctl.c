pmem/ndctl

commit c64cc150a21e1b650f4e88b63ec2827c1e9e3796

ndctl: add support in libndctl to provide deep flush
Providing an API call in libndctl to support accessing the region deep_flush
in sysfs.

Signed-off-by: Dave Jiang <dave.jiang@intel.com>
Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>

ndctl/lib/libndctl.c
@@ -181,6 +181,8 @@ struct ndctl_region {
 	FILE *badblocks;
 	struct badblock bb;
 	enum ndctl_persistence_domain persistence_domain;
+	/* file descriptor for deep flush sysfs entry */
+	int flush_fd;
 };
 
 /**
@@ -511,6 +513,8 @@ static void free_region(struct ndctl_region *region)
 	free(region->region_path);
 	if (region->badblocks)
 		fclose(region->badblocks);
+	if (region->flush_fd > 0)
+		close(region->flush_fd);
 	free(region);
 }
 
@@ -1048,6 +1052,14 @@ NDCTL_EXPORT unsigned long long ndctl_region_get_resource(struct ndctl_region *r
 
 	return strtoull(buf, NULL, 0);
 }
+
+NDCTL_EXPORT int ndctl_region_deep_flush(struct ndctl_region *region)
+{
+	int rc = pwrite(region->flush_fd, "1\n", 1, 0);
+
+	return (rc == -1) ? -errno : 0;
+}
+
 
 NDCTL_EXPORT const char *ndctl_bus_get_cmd_name(struct ndctl_bus *bus, int cmd)
 {
@@ -1828,6 +1840,7 @@ static void *add_region(void *parent, int id, const char *region_base)
 	struct ndctl_bus *bus = parent;
 	struct ndctl_ctx *ctx = bus->ctx;
 	char *path = calloc(1, strlen(region_base) + 100);
+	int perm;
 
 	if (!path)
 		return NULL;
@@ -1903,6 +1916,24 @@ static void *add_region(void *parent, int id, const char *region_base)
 	else
 		region->persistence_domain = region_get_pd_type(buf);
 
+	sprintf(path, "%s/deep_flush", region_base);
+	region->flush_fd = open(path, O_RDWR | O_CLOEXEC);
+	if (region->flush_fd == -1)
+		goto out;
+
+	if (pread(region->flush_fd, buf, 1, 0) == -1) {
+		close(region->flush_fd);
+		region->flush_fd = -1;
+		goto out;
+	}
+
+	perm = strtol(buf, NULL, 0);
+	if (perm == 0) {
+		region->flush_fd = -1;
+		close(region->flush_fd);
+	}
+
+ out:
 	free(path);
 	return region;
 

ndctl/lib/libndctl.sym
@@ -357,4 +357,5 @@ global:
 	ndctl_namespace_disable_write_cache;
 	ndctl_bus_get_scrub_state;
 	ndctl_bus_start_scrub;
+	ndctl_region_deep_flush;
 } LIBNDCTL_14;

ndctl/libndctl.h
@@ -367,6 +367,7 @@ int ndctl_region_enable(struct ndctl_region *region);
 int ndctl_region_disable_invalidate(struct ndctl_region *region);
 int ndctl_region_disable_preserve(struct ndctl_region *region);
 void ndctl_region_cleanup(struct ndctl_region *region);
+int ndctl_region_deep_flush(struct ndctl_region *region);
 
 struct ndctl_interleave_set;
 struct ndctl_interleave_set *ndctl_region_get_interleave_set(

commit 0d8020825520b296ee602a26b57fba9a1951f2fe

ndctl: fix ordering of closing vs invalidating flush_fd
Static analysis warns that we may be closing a negative 'fd'. Fix this
by closing before invalidating the flush_fd with a -1 in add_region()

Fixes: commit c64cc15 ("ndctl: add support in libndctl to provide deep flush")
Cc: Dave Jiang <dave.jiang@intel.com>
Acked-by: Dave Jiang <dave.jiang@intel.com>
Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>

ndctl/lib/libndctl.c
@@ -1929,8 +1929,8 @@ static void *add_region(void *parent, int id, const char *region_base)
 
 	perm = strtol(buf, NULL, 0);
 	if (perm == 0) {
-		region->flush_fd = -1;
 		close(region->flush_fd);
+		region->flush_fd = -1;
 	}
 
  out: