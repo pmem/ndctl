pmem/ndctl

commit dd8e6ff9ab00ce4735ed95ccb77eb9435633616d

ndctl: Create ndctl udev rules for dirty shutdown
This patch provides an ndctl udev rule and the program it runs. The rule
sends two ndctl commands to allow applications to manage unsafe shutdowns.

The first command acknowledges the shutdown. For nvdimms that support
this, the last shutdown status will remain set to its current status
until it is acknowledged.

The second command retrieves the unclean shutdown count (USC) and saves it
in a known location. Only root can directly access the health's shutdown
count, so we have to stash it somewhere accessible to non-privileged
users. A successful execution of the rule will write USC to the run time
tmpfs location. By default, the location will be set to:

  /run/ndctl/nmem<X>/usc

A distro may change this location using the '--with-tmpfilesdir=[DIR]'.

Reading the file will report the count observed when the dimm was
added.

Signed-off-by: Keith Busch <keith.busch@intel.com>
Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>

ndctl/ndctl-udev.c
@@ -1,0 +1,147 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright(c) 2018 Intel Corporation. All rights reserved. */
+
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <ndctl/libndctl.h>
+
+/**
+ * mkdir_p
+ *
+ * Copied from util-linux lib/fileutils.c
+ */
+static int mkdir_p(const char *path, mode_t mode)
+{
+	char *p, *dir;
+	int rc = 0;
+
+	if (!path || !*path)
+		return -EINVAL;
+
+	dir = p = strdup(path);
+	if (!dir)
+		return -ENOMEM;
+
+	if (*p == '/')
+		p++;
+
+	while (p && *p) {
+		char *e = strchr(p, '/');
+		if (e)
+			*e = '\0';
+		if (*p) {
+			rc = mkdir(dir, mode);
+			if (rc && errno != EEXIST)
+				break;
+			rc = 0;
+		}
+		if (!e)
+			break;
+		*e = '/';
+		p = e + 1;
+	}
+
+	free(dir);
+	return rc;
+}
+
+static struct ndctl_dimm *find_dimm(struct ndctl_ctx *ctx, const char *devname)
+{
+	struct ndctl_bus *bus;
+	struct ndctl_dimm *dimm;
+
+	ndctl_bus_foreach(ctx, bus) {
+		ndctl_dimm_foreach(bus, dimm) {
+			if (strcmp(ndctl_dimm_get_devname(dimm), devname) == 0)
+				return dimm;
+		}
+	}
+	return NULL;
+}
+
+static void ack_shutdown(struct ndctl_dimm *dimm)
+{
+	struct ndctl_cmd *cmd;
+
+	cmd = ndctl_dimm_cmd_new_ack_shutdown_count(dimm);
+	if (!cmd)
+		return;
+	ndctl_cmd_submit(cmd);
+	ndctl_cmd_unref(cmd);
+}
+
+static void save_unsafe_shutdown_count(struct ndctl_dimm *dimm,
+				       const char *devname)
+{
+	char *path, *usc, count[16];
+	unsigned int shutdown;
+	struct ndctl_cmd *cmd;
+	int fd;
+
+	cmd = ndctl_dimm_cmd_new_smart(dimm);
+	if (!cmd)
+		return;
+
+	if (ndctl_cmd_submit(cmd))
+		goto unref_cmd;
+
+	shutdown = ndctl_cmd_smart_get_shutdown_count(cmd);
+	if (shutdown == UINT_MAX)
+		goto unref_cmd;
+
+	if (asprintf(&path, DEF_TMPFS_DIR "/%s", devname) < 0)
+		goto unref_cmd;
+
+	if (mkdir_p(path, 0755))
+		goto free_path;
+
+	if (asprintf(&usc, "%s/usc", path) < 0)
+		goto free_path;
+
+	fd = open(usc, O_WRONLY | O_CREAT, 0644);
+	if (fd < 0)
+		goto free_usc;
+
+	if (snprintf(count, sizeof(count), "%u\n", shutdown) < 0)
+		goto free_usc;
+
+	if (write(fd, count, strlen(count)) < 0)
+		goto free_usc;
+ free_usc:
+	free(usc);
+ free_path:
+	free(path);
+ unref_cmd:
+	ndctl_cmd_unref(cmd);
+}
+
+int main(int argc, char *argv[])
+{
+	struct ndctl_ctx *ctx;
+	struct ndctl_dimm *dimm = NULL;
+	const char *devname;
+
+	if (argc < 2)
+		return EINVAL;
+
+	devname = argv[1];
+	if (ndctl_new(&ctx))
+		return ENOMEM;
+
+	dimm = find_dimm(ctx, devname);
+	if (!dimm)
+		return ENODEV;
+
+	ack_shutdown(dimm);
+	save_unsafe_shutdown_count(dimm, devname);
+
+	ndctl_unref(ctx);
+	return 0;
+}

.gitignore
@@ -25,6 +25,7 @@ daxctl/lib/libdaxctl.pc
 *.a
 ndctl/lib/libndctl.pc
 ndctl/ndctl
+ndctl/ndctl-udev
 rhel/
 sles/ndctl.spec
 util/log.lo

Makefile.am
@@ -42,6 +42,9 @@ bashcompletiondir = $(BASH_COMPLETION_DIR)
 dist_bashcompletion_DATA = contrib/ndctl
 endif
 
+udevrulesdir = $(UDEVDIR)/rules.d
+dist_udevrules_DATA = contrib/80-ndctl.rules
+
 noinst_LIBRARIES = libccan.a
 libccan_a_SOURCES = \
 	ccan/str/str.h \

configure.ac
@@ -164,8 +164,17 @@ AS_IF([test "x$with_systemd_unit_dir" != "xno"],
 	[AC_SUBST([systemd_unitdir], [$with_systemd_unit_dir])])
 AM_CONDITIONAL([ENABLE_SYSTEMD_UNIT_DIR], [test "x$with_systemd_unit_dir" != "xno"])
 
+AC_ARG_WITH([tmpfilesdir],
+	[AS_HELP_STRING([--with-tmpfilesdir=DIR], [Directory for temporary runtime files])],
+	[tmpfilesdir=$withval],
+	[tmpfilesdir="/run"])
+
+UDEVDIR="$(pkg-config udev --variable=udevdir)"
+AC_SUBST([UDEVDIR])
+
 my_CFLAGS="\
 -D DEF_CONF_FILE='\"${sysconfdir}/ndctl/monitor.conf\"' \
+-D DEF_TMPFS_DIR='\"${tmpfilesdir}/ndctl\"' \
 -Wall \
 -Wchar-subscripts \
 -Wformat-security \
@@ -207,6 +216,7 @@ AC_MSG_RESULT([
         libdir:                 ${libdir}
         includedir:             ${includedir}
 	systemd-unit-dir:	${systemd_unitdir}
+        tmpfilesdir:            ${tmpfilesdir}
 
         compiler:               ${CC}
         cflags:                 ${CFLAGS}

contrib/80-ndctl.rules
@@ -1,0 +1,3 @@
+# do not edit this file, it will be overwritten on update
+
+ACTION=="add", KERNEL=="nmem*", RUN+="ndctl-udev $kernel"

ndctl.spec.in
@@ -110,6 +110,7 @@ make check
 %postun -n DAX_LNAME -p /sbin/ldconfig
 
 %define bashcompdir %(pkg-config --variable=completionsdir bash-completion)
+%define udevdir  %(pkg-config --variable=udevdir udev)
 
 %files
 %defattr(-,root,root)
@@ -119,6 +120,8 @@ make check
 %{bashcompdir}/
 %{_sysconfdir}/ndctl/monitor.conf
 %{_unitdir}/ndctl-monitor.service
+%{_udevrulesdir}/80-ndctl.rules
+%{udevdir}/ndctl-udev
 
 %files -n daxctl
 %defattr(-,root,root)

ndctl/Makefile.am
@@ -51,3 +51,8 @@ EXTRA_DIST += $(monitor_config_file)
 if ENABLE_SYSTEMD_UNIT_DIR
 systemd_unit_DATA = ndctl-monitor.service
 endif
+
+ndctl_udevdir = $(UDEVDIR)
+ndctl_udev_PROGRAMS = ndctl-udev
+ndctl_udev_SOURCES = ndctl-udev.c
+ndctl_udev_LDADD = lib/libndctl.la

commit bd45c85009225bd76765cb911872b9cd0ba263ba

ndctl, udev: fix a resource leak in save_unsafe_shutdown_count
Static analysis reports that we leak the 'fd' in the above function. Fix
by adding a proper cleanup path that closes it.

Cc: Keith Busch <keith.busch@intel.com>
Fixes: dd8e6ff9ab00 ("ndctl: Create ndctl udev rules for dirty shutdown")
Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>

ndctl/ndctl-udev.c
@@ -110,10 +110,13 @@ static void save_unsafe_shutdown_count(struct ndctl_dimm *dimm,
 		goto free_usc;
 
 	if (snprintf(count, sizeof(count), "%u\n", shutdown) < 0)
-		goto free_usc;
+		goto close_fd;
 
 	if (write(fd, count, strlen(count)) < 0)
-		goto free_usc;
+		goto close_fd;
+
+ close_fd:
+	close(fd);
  free_usc:
 	free(usc);
  free_path: