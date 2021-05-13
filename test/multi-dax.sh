fe831b526b88f6ca7a27fdb149b8a7d2ecddbc55
Santosh Sivaraj <santosh@fossix.org>
2021-05-13 11:42:18 +0530

    Use page size as alignment value
    The alignment sizes passed to ndctl in the tests are all hardcoded to 4k,
    the default page size on x86. Change those to the default page size on that
    architecture (sysconf/getconf). No functional changes otherwise.
    
    Link: https://lore.kernel.org/r/20210513061218.760322-4-santosh@fossix.org
    Signed-off-by: Santosh Sivaraj <santosh@fossix.org>
    Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>

--- test/multi-dax.sh
+++ test/multi-dax.sh
@@ -13,6 +13,8 @@ check_prereq "jq"
 
 trap 'err $LINENO' ERR
 
+ALIGN_SIZE=`getconf PAGESIZE`
+
 # setup (reset nfit_test dimms)
 modprobe nfit_test
 $NDCTL disable-region -b $NFIT_TEST_BUS0 all
@@ -23,9 +25,9 @@ rc=1
 query=". | sort_by(.available_size) | reverse | .[0].dev"
 region=$($NDCTL list -b $NFIT_TEST_BUS0 -t pmem -Ri | jq -r "$query")
 
-json=$($NDCTL create-namespace -b $NFIT_TEST_BUS0 -r $region -t pmem -m devdax -a 4096 -s 16M)
+json=$($NDCTL create-namespace -b $NFIT_TEST_BUS0 -r $region -t pmem -m devdax -a $ALIGN_SIZE -s 16M)
 chardev1=$(echo $json | jq ". | select(.mode == \"devdax\") | .daxregion.devices[0].chardev")
-json=$($NDCTL create-namespace -b $NFIT_TEST_BUS0 -r $region -t pmem -m devdax -a 4096 -s 16M)
+json=$($NDCTL create-namespace -b $NFIT_TEST_BUS0 -r $region -t pmem -m devdax -a $ALIGN_SIZE -s 16M)
 chardev2=$(echo $json | jq ". | select(.mode == \"devdax\") | .daxregion.devices[0].chardev")
 
 _cleanup
