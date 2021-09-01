2db20301b3f5096cfcf7bfb9d41cd78fbdc49f8b
Dan Williams <dan.j.williams@intel.com>
2021-09-01 00:37:07 -0700

    build: Add meson rpmbuild support
    Beyond being a prerequisite for removing autotools support, this capability
    served as validation that the meson conversion generated all the same files
    as autotools and installed them to the same expected locations.
    
    The procedure to use the rpmbuild.sh script is:
    
        meson setup build
        meson compile -C build rhel/ndctl.spec
        ./rpmbuild.sh build/rhel/ndctl.spec
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- rpmbuild.sh
+++ rpmbuild.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+spec=${1:-$(dirname $0)/rhel/ndctl.spec)}
+
 pushd $(dirname $0) >/dev/null
 [ ! -d ~/rpmbuild/SOURCES ] && echo "rpmdev tree not found" && exit 1
 ./make-git-snapshot.sh
 popd > /dev/null
-rpmbuild -ba $(dirname $0)/rhel/ndctl.spec
+rpmbuild --nocheck -ba $spec
