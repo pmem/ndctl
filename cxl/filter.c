0ce0152d8e29f85325a3a59f94051228540abf6a
Dan Williams <dan.j.williams@intel.com>
2022-01-23 16:52:20 -0800

    cxl/list: Support multiple memdev device name filter arguments
    Similar to 'ndctl list', allow for a syntax like:
    
        cxl list -m "$(seq -s ' ' 2 5)"
    
    ...to filter the output to just those 4 memdevs.
    
    Link: https://lore.kernel.org/r/164298554075.3021641.17678360870961637912.stgit@dwillia2-desk3.amr.corp.intel.com
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Vishal Verma <vishal.l.verma@intel.com>

--- cxl/filter.c
+++ cxl/filter.c
@@ -2,24 +2,40 @@
 // Copyright (C) 2015-2020 Intel Corporation. All rights reserved.
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <cxl/libcxl.h>
 #include "filter.h"
 
 struct cxl_memdev *util_cxl_memdev_filter(struct cxl_memdev *memdev,
-                                         const char *ident)
+					  const char *__ident)
 {
-       int memdev_id;
+	char *ident, *save;
+	const char *name;
+	int memdev_id;
 
-       if (!ident || strcmp(ident, "all") == 0)
-               return memdev;
+	if (!__ident)
+		return memdev;
 
-       if (strcmp(ident, cxl_memdev_get_devname(memdev)) == 0)
-               return memdev;
+	ident = strdup(__ident);
+	if (!ident)
+		return NULL;
 
-       if ((sscanf(ident, "%d", &memdev_id) == 1
-                       || sscanf(ident, "mem%d", &memdev_id) == 1)
-                       && cxl_memdev_get_id(memdev) == memdev_id)
-               return memdev;
+	for (name = strtok_r(ident, " ", &save); name;
+	     name = strtok_r(NULL, " ", &save)) {
+		if (strcmp(name, "all") == 0)
+			break;
 
-       return NULL;
+		if ((sscanf(name, "%d", &memdev_id) == 1 ||
+		     sscanf(name, "mem%d", &memdev_id) == 1) &&
+		    cxl_memdev_get_id(memdev) == memdev_id)
+			break;
+
+		if (strcmp(name, cxl_memdev_get_devname(memdev)) == 0)
+			break;
+	}
+
+	free(ident);
+	if (name)
+		return memdev;
+	return NULL;
 }
