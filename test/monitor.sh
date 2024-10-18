2e78b22f0e72f1641cedd3843ec7547c897f09cf
Li Zhijian <lizhijian@fujitsu.com>
2024-10-18 09:30:19 +0800

    test/monitor.sh: convert float to integer before increment
    The test log reported:
    test/monitor.sh: line 149: 40.0: syntax error: invalid arithmetic operator (error token is ".0")
    
    It does stop the test prematurely. We never run the temperature
    inject test case of test_filter_dimmevent() because of the inability
    to increment the float.
    
    Signed-off-by: Li Zhijian <lizhijian@fujitsu.com>
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>
    Link: https://lore.kernel.org/r/20241018013020.2523845-1-lizhijian@fujitsu.com
    Signed-off-by: Alison Schofield <alison.schofield@intel.com>

--- test/monitor.sh
+++ test/monitor.sh
@@ -146,6 +146,7 @@ test_filter_dimmevent()
 	stop_monitor
 
 	inject_value=$($NDCTL list -H -d $monitor_dimms | jq -r .[]."health"."temperature_threshold")
+	inject_value=${inject_value%.*}
 	inject_value=$((inject_value + 1))
 	start_monitor "-d $monitor_dimms -D dimm-media-temperature"
 	inject_smart "-m $inject_value"
